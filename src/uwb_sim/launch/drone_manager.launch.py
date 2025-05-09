bdd3e550fa4152e3ec1baf300aacb0585730d311
Porofly <yeogyeom1@naver.com>
2025-05-09 13:13:38 +0900

    feat: Add launch argument for ros2 bag recording in drone_manager

--- src/uwb_sim/launch/drone_manager.launch.py
+++ src/uwb_sim/launch/drone_manager.launch.py
@@ -1,12 +1,14 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.substitutions import FindExecutable
+from launch.substitutions import FindExecutable, LaunchConfiguration
 from launch_ros.actions import Node
-from launch.actions import ExecuteProcess, OpaqueFunction, IncludeLaunchDescription
+from launch.actions import ExecuteProcess, OpaqueFunction, IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def launch_setup(context, *args, **kwargs):
+    do_record = context.launch_configurations.get('record', 'false').lower() in ('true', '1')
+
     # MicroXRCEAgent udp4 -p 8888
     xrce_agent_process = ExecuteProcess(
         cmd=[FindExecutable(name='MicroXRCEAgent'), 'udp4', '-p', '8888'],
@@ -46,10 +48,18 @@ def launch_setup(context, *args, **kwargs):
         xrce_agent_process,
         drone_manager_node,
         linktrack_launch,
-        bag_record,
     ]
+
+    if do_record:
+        nodes_to_start.append(bag_record)
 
     return nodes_to_start
 
 def generate_launch_description():
-    return LaunchDescription([OpaqueFunction(function=launch_setup)])
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'record',
+            default_value='false',
+        ),
+        OpaqueFunction(function=launch_setup)
+    ])
