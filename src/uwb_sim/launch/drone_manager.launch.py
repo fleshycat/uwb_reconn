2bcbf8ec844a25de3278536e5d6f78942b0bc7fe
Porofly <yeogyeom1@naver.com>
2025-06-01 04:34:23 +0000

    update system_id

--- src/uwb_sim/launch/drone_manager.launch.py
+++ src/uwb_sim/launch/drone_manager.launch.py
@@ -1,13 +1,16 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.substitutions import FindExecutable, LaunchConfiguration
+from launch.substitutions import FindExecutable
 from launch_ros.actions import Node
 from launch.actions import ExecuteProcess, OpaqueFunction, IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def launch_setup(context, *args, **kwargs):
     do_record = context.launch_configurations.get('record', 'false').lower() in ('true', '1')
+    
+    # System ID for the drone
+    system_id = context.launch_configurations.get('system_id', '1')
 
     # MicroXRCEAgent udp4 -p 8888
     xrce_agent_process = ExecuteProcess(
@@ -20,7 +23,7 @@ def launch_setup(context, *args, **kwargs):
         package='drone_manager',
         executable='drone_manager',
         name='drone_manager',
-        parameters=[{'system_id': 1}],
+        parameters=[{'system_id': int(system_id)}],
         output='screen'
     )
 
@@ -32,17 +35,8 @@ def launch_setup(context, *args, **kwargs):
                 'launch',
                 'linktrack.launch.py'
             )
-        )
-    )
-    
-    # ros2 bag record
-    bag_record = ExecuteProcess(
-        cmd=[
-            'ros2', 'bag', 'record',
-            '/drone1/manager/out/ranging',
-            '/drone1/manager/out/monitoring',
-        ],
-        output='screen',
+        ),
+        launch_arguments={'system_id': system_id}.items()
     )
     
     nodes_to_start = [
@@ -52,6 +46,16 @@ def launch_setup(context, *args, **kwargs):
     ]
 
     if do_record:
+        ranging_topic = f'/drone{system_id}/manager/out/ranging'
+        monitoring_topic = f'/drone{system_id}/manager/out/monitoring'
+        bag_record = ExecuteProcess(
+            cmd=[
+                'ros2', 'bag', 'record',
+                ranging_topic,
+                monitoring_topic,
+            ],
+            output='screen',
+        )
         nodes_to_start.append(bag_record)
 
     return nodes_to_start
@@ -61,6 +65,11 @@ def generate_launch_description():
         DeclareLaunchArgument(
             'record',
             default_value='false',
+        ),
+        DeclareLaunchArgument(
+            'system_id',
+            default_value='1',
+            description='Drone System ID'
         ),
         OpaqueFunction(function=launch_setup)
     ])
